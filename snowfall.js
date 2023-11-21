577fe0bd374af0685b647bc79e87c6f956917e86
Usernamenotavailable12 <129051734+Usernamenotavailable12@users.noreply.github.com>
2023-11-21 12:32:48 +0400

    Update snowfall.js

--- snowfall.js
+++ snowfall.js
@@ -1,34 +1,32 @@
-    // Function to create a snowflake
-    function createSnowflake() {
-        const snowflake = document.createElement("div");
-        snowflake.innerHTML = "❄";
-        snowflake.style.position = "fixed";
-        snowflake.style.color = "#fff";
-        snowflake.style.pointerEvents = "none";
-        snowflake.style.left = Math.random() * window.innerWidth + "px";
-        snowflake.style.top = "0px";
-        document.body.appendChild(snowflake);
-  
-        // Animation function
-        function animateSnowflake() {
-          const snowflakeSpeed = 2; // Adjust the falling speed as needed
-          let currentTop = parseInt(snowflake.style.top);
-          snowflake.style.top = currentTop + snowflakeSpeed + "px";
-  
-          // Remove snowflake when it goes beyond the bottom of the screen
-          if (currentTop > window.innerHeight) {
-            document.body.removeChild(snowflake);
-          }
+function createSnowflake() {
+    const snowflake = document.createElement("div");
+    snowflake.innerHTML = "❄";
+    snowflake.style.position = "fixed";
+    snowflake.style.color = "#fff";
+    snowflake.style.pointerEvents = "none";
+    snowflake.style.left = Math.random() * window.innerWidth + "px";
+    snowflake.style.top = "0px";
+    document.body.appendChild(snowflake);
+
+    function animateSnowflake() {
+        const snowflakeSpeed = 2; // Adjust the falling speed as needed
+        let currentTop = parseInt(snowflake.style.top);
+        snowflake.style.top = currentTop + snowflakeSpeed + "px";
+
+        // Check if the snowflake is still a child of the document
+        if (document.body.contains(snowflake)) {
+            // Remove snowflake when it goes beyond the bottom of the screen
+            if (currentTop > window.innerHeight) {
+                document.body.removeChild(snowflake);
+            }
         }
-  
-        // Set up the animation
-        setInterval(animateSnowflake, 10); // Adjust the interval as needed
-      }
-  
-      // Function to start snowfall
-      function startSnowfall() {
-        setInterval(createSnowflake, 500); // Adjust the interval as needed
-      }
-  
-      // Start snowfall when the page is loaded
-      window.onload = startSnowfall;
+    }
+
+    setInterval(animateSnowflake, 10); // Adjust the interval as needed
+}
+
+function startSnowfall() {
+    setInterval(createSnowflake, 500); // Adjust the interval as needed
+}
+
+window.onload = startSnowfall;
